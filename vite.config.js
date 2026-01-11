0e7aeb88fa03f780a8adfdeb631881707ef551b7
karthi2905 <karthikeyan29.5.2006@gmail.com>
2026-01-11 22:36:11 +0530

    Configure Vite to copy .nojekyll file for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: '/',
+    publicDir: 'public', // Ensures .nojekyll and other public files are copied to dist
 })
